f5880b89f6d1e8c0507901e151cea65172f72ff7
joeydifrancesco <slimbootytang@gmail.com>
2021-10-08 19:03:58 -0400

    Updating Lab 6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,3 +1,5 @@
+async function windowActions () {
+
 const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'
 
 const cities = []
@@ -10,12 +12,22 @@ function findMatches(wordToMatch, cities) {
     return cities.filter(place => {
         const regex = RegExp(wordToMatch, 'gi');
         return place.city.match(regex) || place.state.match(regex)
-    })
+    });
 }
 
 function displayMatches () {
     const matchArray = findMatches(this.value, cities)
-    console.log(matchArray)
+    const html = matchArray.map(place => {
+        return `
+            <li>
+                <span class="name">${place.name}</span>
+                <span class="address">${place.address_line_1}</span>
+                <span class="city">${place.city}</span>
+                <span class="zip">${place.zip}</span>
+            </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
 }
 
 const searchInput = document.querySelector('.search')
@@ -23,4 +35,7 @@ const suggestions = document.querySelector('.suggestions')
 
 searchInput.addEventListener('change', displayMatches)
 searchInput.addEventListener('keyup', (evt) => { displayMatches(evt);})
-})
+
+}
+
+window.onload = windowActions;
